acd1b4d8bf1609b3b3acd86dca4733d73e70b4e2
vaibhavkalra009 <vaibhavkalra009@gmail.com>
2025-06-07 07:22:52 +0530

    hero section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-console.log("E-Commerce Website Loaded");
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+});
